bbc8fd5490b807cabf2f2ba2634e979521ec8e7f
Frederick N. Numbisi <38797347+Frederick-Numbisi@users.noreply.github.com>
2021-04-08 07:11:08 +0200

    Update Ripley_SpatialPoint_Analysis.R

--- Ripley_SpatialPoint_Analysis.R
+++ Ripley_SpatialPoint_Analysis.R
@@ -4,15 +4,15 @@ library(spatstat)
 library(maptools)
 library(ggplot2)
 
-setwd("D:/...../...../Bakoa_TreesClass4") # Select location of dataset as the working directory
+setwd("Path/to/the/working/Directory") # Select location of dataset as the working directory
 
 # -----------------------------------------
 # Analysis procedure for data in each plot
 # -----------------------------------------
-# Sample code for plot
+# Sample code for analysing trees in a sample plot
 
 Plot_Data = read.csv("Plot_tclass4.csv") # Read the data sete
-Plot_Data$TTP = as.factor(Plot23_Data$TTP) # Convert the TTP variable to factor 
+Plot_Data$TTP = as.factor(Plot_Data$TTP) # Convert the TTP variable to factor 
 
 Plot_T4  <- readShapePoints("Plot_tclass4.shp") # Read the spatial data (shapefile) of tree point locations
 SP <- as(Plot_T4, "SpatialPoints") # Convert the spatial data to spatial point patterns
@@ -21,7 +21,7 @@ P  <- as(SP, "ppp")
 marks(P) = Plot_Data[5]
 
 # Get Plot shapefile
-S2  <- readShapePoly("D:/PhD Manuscript/...../Plot23.shp")
+S2  <- readShapePoly("D:/PhD Manuscript/...../Plot.shp")
 SP2 <- as(S2, "SpatialPolygons")
 W   <- as(SP2, "owin")
 
@@ -35,10 +35,9 @@ a = plot(P, main="Plot All Trees")
 plot(density(P))
 #legend(0.25, 0.5, names(a), pch=a)
 
-#Set measurement unit of window()
-#unitname(P) <- "meter"
 
-## The Kdot(r) inhomogenous function for each tree group within stand
+
+## Compute the Kdot(r) inhomogenous function for each tree group within stand
 
 # method (1): estimate intensities by nonparametric smoothing
 Co <- split(P)$Co
@@ -114,7 +113,7 @@ plot(Lij_Plot, . - r ~ r, xlab="Distance (m)", ylab="Lij(r)", main="Lij inhom. F
 Lij_Plot$iso  <- Lij_Plot$iso  - Lij_Plot$r
 Lij_Plot$theo <- Lij_Plot$theo - Lij_Plot$r
 
-write.csv(Lij_Plot, file = "Lij_Bivariate_inhom_Plot23_Co3_to_AsT.csv", row.names=TRUE)
+write.csv(Lij_Plot, file = "Lij_Bivariate_inhom_Plot_Co3_to_AsT.csv", row.names=TRUE)
 
 
 
@@ -123,75 +122,23 @@ write.csv(Lij_Plot, file = "Lij_Bivariate_inhom_Plot23_Co3_to_AsT.csv", row.name
 # SAVING AND APPENDING THE Ripley-K RESULTS AND FILES IN CSV
 #--------------------------------------------------
 
-#A) Appending Multivariate Lij(r) Results
+#Appending Multivariate Lij(r) Results
+file_path_dir <- "/path/to/the/directory/of/Lij(r)Results/"
 
-Lij_Results <- c(Lij_Plot2, Lij_Plot3, Lij_Plot4, Lij_Plot5, Lij_Plot6, Lij_Plot8,
-                 Lij_Plot10, Lij_Plot11, Lij_Plot12, Lij_Plot13, Lij_Plot21, 
-                 Lij_Plot22, Lij_Plot23, Lij_Plot26, Lij_Plot28, Lij_Plot29,
-                 Lij_Plot34, Lij_Plot38, Lij_Plot42, Lij_Plot43, Lij_Plot44, Lij_Plot50)
+#Create function to merge results
+RipleyKmerge <- function(mydirectory){
+  filenames <- list.files(path = mydirectory, full.names = TRUE)
+  Lijlist <- lapply(filenames, function(x){
+    read.csv(file = x, header = TRUE)}
+                   )
+  Reduce(function(x,y) {
+    merge(x, y, all = TRUE)}, Lijlist)
+}
 
+# Run function using directory of results
+Lij_Results <- RipleyKmerge(file_path_dir)
+write.csv(Lij_Results, "Lij_Results_Cocoa-AsT.csv", row.names = TRUE)
 
-write.csv(Lij_Results, "Lij_Results_Co3-AsT.csv", row.names = TRUE)
-
-
-#B) Appending pcf-g(r) results
-
-PCFij_Results <- c(gij_Plot2, gij_Plot3, gij_Plot4, gij_Plot5, gij_Plot6, gij_Plot8,
-                   gij_Plot10, gij_Plot11, gij_Plot12, gij_Plot13, gij_Plot21,  
-                   gij_Plot22, gij_Plot23, gij_Plot26, gij_Plot28, gij_Plot29, 
-                   gij_Plot34, pc_Plot38, pc_Plot42, pc_Plot43, pc_Plot44, pc_Plot50)
-
-
-write.csv(PCFij_Results, "PCFij_Results_Co3-AsT.csv", row.names = TRUE)
-
-
-
-#-------------------------------------------------------------------------
-#-------------------------------------------------------------------------
-#
-# CREATE HYPERFRAME POINT PATTERN SPATIAL DATA FOR COCOA AND ASSOCIATED TREES
-#
-#-------------------------------------------------------------------------
-
-library(sp)
-library(spatstat)
-library(maptools)
-library(ggplot2)
-
-setwd("/Path/to/the/...../Working/Directory")
-
-# The analysis procedure is replicated for all plots using a function call
-# Case analysis for a Plot
-
-Plot_Data = read.csv("Plot_tclass4.csv")
-Plot_Data$TTP = as.factor(Plot_Data$TTP)
-
-Plot_T4  <- readShapePoints("Plot_tclass4.shp")
-SP <- as(Plot_T4, "SpatialPoints")
-P  <- as(SP, "ppp")
-#Add marks to P(data)
-marks(P) = Plot_Data[5]
-
-# Get Plot shapefile
-S2  <- readShapePoly("/Directory/to/...../Plot.shp")
-SP2 <- as(S2, "SpatialPolygons")
-W   <- as(SP2, "owin")
-
-# Apply Plot window (polygon) to trees in Plot
-P$window <- W
-plot(P, pch=16, main="Plot All Trees")
-summary(P)
-plot(split(P))
-plot(density(split(P)))
-a = plot(P, main="Plot All Trees")
-plot(density(P))
-
-PT4 = P
-summary(PT4)
-
-PT4Co = unmark(split(PT4)$Co) # To subset just the cocoa trees 
-PT4AsT = unmark(split(PT4)$AsT) # To subset jut Associated trees
-PT4All = unmark(PT4) # Remove marks (tree type) from point pattern data
 
 
 
